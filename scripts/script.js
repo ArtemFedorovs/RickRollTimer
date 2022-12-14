22658f1b5d624e4f597dd837a605ec9b0d7f271a
ARTEM FEDOROV <fedorov4991@gmail.com>
2022-12-14 16:33:13 +0300

    url bug fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let url
 function counts (){
- url=location.href.slice(0, (location.href.length-10))+"timer.html?time="+Date.parse(document.getElementById("date").value);
+ url=location.href.replace('index.html', 'timer.html?time=' + Date.parse(document.getElementById("date").value));
  document.getElementById("output").value=url;
 };
 
